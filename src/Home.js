f7bf6df254aae4ad0ffb60ba24467aaf951187bc
Felipe G <pipe2705@gmail.com>
2019-12-15 17:35:09 -0800

    Changes name of app.js to home.js and does sanity check.

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return <h1 className="jumbotron-heading">Giphy Search</h1>;
+  }
+}
+
+export default Home;
